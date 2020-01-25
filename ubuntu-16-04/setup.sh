634470a61097b4ac3925533f70003fc740c4c522
Vlad Khazin <vladimir.khazin@icssolutions.ca>
2020-01-24 20:29:46 -0500

    Update setup.sh

--- ubuntu-16-04/setup.sh
+++ ubuntu-16-04/setup.sh
@@ -89,7 +89,7 @@ sudo bash -c "iptables-save > /etc/iptables/rules.v4"
 echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
 echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
 sudo -E apt-get install iptables-persistent -y
-sudo -E service iptables-persistent start
+sudo service netfilter-persistent start
 #####################################################################
 
 #####################################################################
